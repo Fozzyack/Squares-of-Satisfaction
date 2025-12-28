7ea660828933ecfe5647f24279d4ddbe15a26fe0
Frasier <fsundra@gmail.com>
2025-12-28 23:38:52 +0800

    Adds super basic middleware (no auth)

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -4,10 +4,17 @@ import (
 	"backend/app"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func SetupRoutes(app *app.Application) *chi.Mux {
+
 	r := chi.NewRouter()
+
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
 
 	r.Get("/health", app.HealthCheck)
 	r.Get("/squares/{id}", app.SquareHandler.HandleGetSquareByID)
