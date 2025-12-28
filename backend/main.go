c23f051caf9726de02cda6dc52479b23bc4edccf
Frasier <fsundra@gmail.com>
2025-12-28 13:48:35 +0800

    Adds a healthcheck

--- backend/main.go
+++ backend/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"backend/app"
+	"flag"
+	"fmt"
 	"net/http"
 	"time"
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8090, "go backend server port")
+	flag.Parse()
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("App Started")
+	app.Logger.Printf("App Starting on Port %d", port)
 
+	http.HandleFunc("/health", HealthCheck)
 	server := &http.Server{
-		Addr:         ":8090",
+		Addr:         fmt.Sprintf(":%d", port),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
@@ -25,4 +31,8 @@ func main() {
 	if err != nil {
 		app.Logger.Fatal("There was an issue running the server")
 	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is Available\n")
 }
