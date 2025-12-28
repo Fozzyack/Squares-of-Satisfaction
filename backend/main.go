3953ee952270f0a36150c9744ff0458734f30a8f
Frasier <fsundra@gmail.com>
2025-12-28 12:07:37 +0800

    Initial server setup

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"backend/app"
+	"net/http"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("App Started")
+
+	server := &http.Server{
+		Addr:         ":8090",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal("There was an issue running the server")
+	}
 }
