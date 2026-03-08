5180ef4ace5a74ada789c60e278d7f5fbd3709e0
Frasier <fsundra@gmail.com>
2026-03-08 11:38:53 +0800

    adds middleware

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,11 +2,19 @@ package routes
 
 import (
 	"backend/app"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+
 	r.Get("/health", app.HealthCheck)
 	return r
 }
